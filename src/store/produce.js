a689af84bdcd6046552cae231ff0ad2632106125
Eric-Mai-25 <ericmai2@gmail.com>
2023-09-27 14:50:41 -0700

    finisned POPULATE action

--- src/store/produce.js
+++ src/store/produce.js
@@ -1,7 +1,25 @@
+import produceData from "../mockData/produce.json";
 
-export default function produceReducer(state = {}, action){
-    switch (action.type) {
-        default:
-            return state;
-    }
+const POPULATE = "POPULATE";
+
+export const populateProduce = () => {
+  return {
+    type: POPULATE,
+    produce: produceData,
+  };
+};
+
+export default function produceReducer(state = {}, action) {
+    const nextState = Object.assign({}, Object.freeze(state))
+
+  switch (action.type) {
+    case POPULATE:
+       action.produce.forEach(produce => {
+        nextState[produce.id] = produce
+       });
+       return nextState
+    default:
+      return state;
+  }
 }
+
